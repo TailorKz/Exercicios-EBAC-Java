d706ddbba40d50b3d85ce84cdfbf9223c9510479
Tailor <tailorkunz@gmail.com>
2025-07-09 10:03:37 -0300

    finalizando módulo aula aula módulo 22

---
+++ Tarefa_Streams/src/com/tailorkz/streams/FiltraGenero.java
@@ -0,0 +1,27 @@
+package com.tailorkz.streams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FiltraGenero {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite os nomes e sexos separados por vírgula. Exemplo: João-M, Maria-F, Juca-M");
+        String valores = scanner.nextLine();
+        String[] pessoas = valores.split(",");
+
+        List<String> feminino = Arrays.stream(pessoas).map(String::trim).filter(pessoa -> {
+            String[] partes = pessoa.split("-");
+            return partes[1].equalsIgnoreCase("F");
+        }).map(pessoa -> pessoa.split("-")[0]).collect(Collectors.toList());
+
+        System.out.println("Lista de pessoas do gênero feminino:");
+        for (String nome : feminino) {
+            System.out.println(nome);
+        }
+    }
+}
